3e35dec1d24d6781f62329bbe37d17bf30d38849
ivan096096 <wanwanich@yandex.ru>
2021-11-23 21:08:31 +0300

    lesson_8

---
+++ src/main/java/Lessons/lesson_8/AGamer.java
@@ -0,0 +1,8 @@
+package Lessons.lesson_8;
+
+public abstract class AGamer {
+    protected String sign;
+
+    abstract boolean shot(int x, int y);
+    abstract boolean win();
+}
---
+++ src/main/java/Lessons/lesson_8/MainClass.java
@@ -0,0 +1,7 @@
+package Lessons.lesson_8;
+
+public class MainClass {
+    public static void main(String[] args) {
+        MainForm gameForm = new MainForm();
+    }
+}
